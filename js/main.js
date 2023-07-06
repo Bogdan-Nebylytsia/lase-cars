19c18516cd278de5cd9ff771da4022b24ce44ace
Bogdan Nebylytsia <bohdan.deal@gmail.com>
2023-07-06 20:35:45 +0300

    removing the swiper slider and replacing it with mine

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
-const swiper = new Swiper(".swiper", {
-   effect: "fade",
-   pagination: {
-     el: ".swiper-pagination",
-   },
-   autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
- });
+let slideIndex = 0;
+const dots = document.querySelectorAll('.slider-pagination-dots');
+const slides = document.querySelectorAll('.slider-slide');
+
+function showSlide() {
+
+  slides.forEach(slide => {
+    slide.classList.remove('showing');
+  });
+
+  slideIndex++;
+
+  if (slideIndex >= slides.length) {
+      slideIndex = 0;
+  }
+
+  slides[slideIndex].classList.add('showing');
+  
+  dots.forEach(dot => {
+    dot.className = dot.className.replace('slider-pagination-dots-active', '');
+  });
+
+  dots[slideIndex].className += ' slider-pagination-dots-active';
+
+
+
+}
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    console.log(index);
+    if (index === 0) {
+      slideIndex = slides.length-1;
+    } else {
+      slideIndex = index-1;
+    }
+
+    console.log(slideIndex);
+    
+    showSlide();
+  });
+});
+
+setInterval(showSlide, 9000);
