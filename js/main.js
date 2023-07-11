a3b7f2a63cf2c06d9e0c3855b568b25e17e147ab
Bogdan Nebylytsia <bohdan.deal@gmail.com>
2023-07-11 22:33:32 +0300

    completion of the new-cars page and tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Slider
+
 let slideIndex = 0;
 const dots = document.querySelectorAll('.slider-pagination-dots');
 const slides = document.querySelectorAll('.slider-slide');
@@ -42,3 +44,30 @@ dots.forEach((dot, index) => {
 });
 
 setInterval(showSlide, 9000);
+
+//tabs
+
+const tabsBtn = document.querySelectorAll('.tabs__btn-item'),
+  tabsContent = document.querySelectorAll('.tabs__content-item');
+
+tabsBtn.forEach(btn => {
+  btn.addEventListener('click', open);
+});
+
+function open(evt) {
+  const tabsTarget = evt.currentTarget,
+    button = tabsTarget.dataset.button;
+  console.log(button);
+
+  tabsBtn.forEach(btn => {
+    btn.classList.remove('tabs__btn-item-active');
+  });
+
+  tabsTarget.classList.add('tabs__btn-item-active');
+
+  tabsContent.forEach(item => {
+    item.classList.remove('tabs__content-item-active');
+  });
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item-active');
+}
